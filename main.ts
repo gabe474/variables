d4e8084df20fd2c666e901af75586b65c7025e02
gabe474 <122543660+gabe474@users.noreply.github.com>
2023-01-26 09:38:51 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,18 @@
+input.onButtonPressed(Button.A, function () {
+    value += 1
+    basic.showNumber(value,-100)
+})
+input.onButtonPressed(Button.AB, function () {
+    value = 0
+    basic.showNumber(value,-100)
+})
+input.onButtonPressed(Button.B, function () {
+    value += -1
+    basic.showNumber(value,-100)
+})
+let value = 0
+value = 0
+basic.showNumber(value,-100)
+basic.forever(function () {
+	
+})
